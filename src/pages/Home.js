0ba3a56eaa61d03db84f8a3b0f87a20569a14717
rendisagala <rendisagala19@gmail.com>
2022-07-30 11:30:04 +0700

    .env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import Hero from "../components/Hero";
+import Explore from "../components/Explore";
 
 function Home() {
   return (
     <div>
       <NavigationBar />
       <Hero />
+      <Explore />
     </div>
   );
 }
